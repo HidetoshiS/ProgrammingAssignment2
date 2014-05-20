9f2dd76b2b56bce710e81b0339adbd777ef727cc
HidetoshiS <sakaue.hidetoshi@tecnos-dsm.co.jp>
2014-05-20 15:51:04 +0900

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -5,13 +5,13 @@
 
 makeCacheMatrix <- function(x = matrix()) {
   s <- NULL  # initialization
-  set <- function(y) { # Set a matrix and initialoze s. (s:inversion matrix)
+  set <- function(y) { # Set a matrix and initialoze 's'. (s:aninversion matrix)
     x <<- y
     s <<- NULL
   }
   get <- function() x  # Print a set matrix
-  setsolve <- function(solve) s <<- solve  # Set a inversion matrix
-  getsolve <- function() s                 # Print a inversion matrix
+  setsolve <- function(solve) s <<- solve  # Set an inversion matrix
+  getsolve <- function() s                 # Print an inversion matrix
   list(set = set, get = get,               # Return (List format)
        setsolve = setsolve,
        getsolve = getsolve)
@@ -22,12 +22,12 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   s <- x$getsolve()  # Get the inversion matrix
-  if(!is.null(s)) {  # If s is not initial, then print s without calculating
+  if(!is.null(s)) {  # If 's' is not initial, then print 's' without calculating
     message("getting cached data")
     return(s)
   }
   data <- x$get()
-  s <- solve(data, ...)  # Calculate a inversion matrix
+  s <- solve(data, ...)  # Calculate an inversion matrix
   x$setsolve(s)          # Set the inversion matrix
   s
 }
